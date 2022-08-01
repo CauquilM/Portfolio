265260270437140db9f534bd6c0abf9794e08368
MathCoder <mathieufc31@gmail.com>
2022-08-01 18:11:08 +0200

    end of contact

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -46,7 +46,7 @@ class Contact extends Component {
                 <SocialMedia theme={theme} />
                 <div className="resume-btn-div">
                   <Button
-                    text="See My Resume"
+                    text="Voir mon CV"
                     newTab={true}
                     href={greeting.resumeLink}
                     theme={theme}
@@ -56,42 +56,8 @@ class Contact extends Component {
             </div>
           </Fade>
           <Fade bottom duration={1000} distance="40px">
-            <div className="blog-heading-div">
-              <div className="blog-heading-text-div">
-                <h1 className="blog-heading-text" style={{ color: theme.text }}>
-                  {blogSection["title"]}
-                </h1>
-                <p
-                  className="blog-header-detail-text subTitle"
-                  style={{ color: theme.secondaryText }}
-                >
-                  {blogSection["subtitle"]}
-                </p>
-                <div className="blogsite-btn-div">
-                  <Button
-                    text="Visit My Blogsite"
-                    newTab={true}
-                    href={blogSection.link}
-                    theme={theme}
-                  />
-                </div>
-              </div>
-              <div className="blog-heading-img-div">
-                {/* <img
-											src={require(`../../assests/images/${blogSection["avatar_image_path"]}`)}
-											alt=""
-										/> */}
-                <BlogsImg theme={theme} />
-              </div>
-            </div>
-          </Fade>
-          <Fade bottom duration={1000} distance="40px">
             <div className="address-heading-div">
               <div className="contact-heading-img-div">
-                {/* <img
-											src={require(`../../assests/images/${addressSection["avatar_image_path"]}`)}
-											alt=""
-										/> */}
                 <AddressImg theme={theme} />
               </div>
               <div className="address-heading-text-div">
@@ -121,7 +87,7 @@ class Contact extends Component {
                 </p>
                 <div className="address-btn-div">
                   <Button
-                    text="Visit on Google Maps"
+                    text="Voir sur Google Maps"
                     newTab={true}
                     href={addressSection.location_map_link}
                     theme={theme}
--- src/portfolio.js
+++ src/portfolio.js
@@ -13,7 +13,7 @@ const seo = {
   og: {
     title: "Mathieu CAUQUIL Portfolio",
     type: "website",
-    url: "https://cauquilm.github.io",
+    url: "https://cauquilm.github.io/Portfolio",
   },
 };
 
@@ -277,9 +277,8 @@ const publicationsHeader = {
 const contactPageData = {
   contactSection: {
     title: "Contact",
-    profile_image_path: "animated_ashutosh.png",
-    description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+    profile_image_path: "moi.jpg",
+    description: "Je suis disponible sur les réseaux suivants",
   },
   blogSection: {
     title: "Blogs",
@@ -289,15 +288,14 @@ const contactPageData = {
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
-    title: "Address",
-    subtitle:
-      "Ambavadi vas, Kanodar, T.A.-Palanpur, Dist.-Banaskantha, Gujarat - 385520",
+    title: "Adresse",
+    subtitle: "4 Impasse Jim Morrison, 31200, Toulouse, France",
     avatar_image_path: "address_image.svg",
-    location_map_link: "https://goo.gl/maps/MpMqtRZytFchMkZ76",
+    location_map_link: "https://goo.gl/maps/1Gpr4vV6qsym331b9",
   },
   phoneSection: {
-    title: "Phone Number",
-    subtitle: "+91 8320758513",
+    title: "Numéro de téléphone",
+    subtitle: "+33-7-86-85-89-48",
   },
 };
 
